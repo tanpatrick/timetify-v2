4a0a16b7115e4e029269258fb441eefd03d6c9c6
Patrick Tan <tanpatrickjoseph@gmail.com>
2017-10-01 12:48:52 +1300

    Add navigation styling using bootstrap

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {
     BrowserRouter as Router,
     Route,
-    Link
+    NavLink
 } from 'react-router-dom'
 
 import About from './about'
@@ -14,26 +14,49 @@ import Users from './users'
 const Routes = () => (
     <Router>
         <div>
-            <nav >
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/projects">Projects</Link>
-                    </li>
-                    <li>
-                        <Link to="/users">Users</Link>
-                    </li>
-                    <li>
-                        <Link to="/settings">Settings</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                </ul>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4 fixed-top bg-faded">
+                <a className="navbar-brand" href="/">
+                    <strong>
+                        timetify
+                    </strong>
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <span className="navbar-text mr-auto">
+                        lorem ipsum dolor sit amet.
+                    </span>
+                    <ul className="navbar-nav mt-2 mt-md-0">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" exact to="/">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" exact to="/projects">
+                                Projects
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" exact to="/users">
+                                Users
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" exact to="/settings">
+                                Settings
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" exact to="/about">
+                                About
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
             </nav>
-            <div>
+            <div className="container-fluid">
                 <Route exact path="/" component={Home} />
                 <Route exact path="/projects" component={Projects} />
                 <Route exact path="/users" component={Users} />
